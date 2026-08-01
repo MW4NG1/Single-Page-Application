78e21a5f979496c1bc5947c058bca35a0e4d56a1
MW4NG1 <msage3803@gmail.com>
2026-08-01 14:09:42 +0300

    feat: restructure App component to include header, project form, search bar, and project list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+// Import React hook
+import { useState } from "react";
+
+// Import components
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import SearchBar from "./components/SearchBar";
+import ProjectList from "./components/ProjectList";
+
 function App() {
+  // Store all projects
+  const [projects, setProjects] = useState([]);
+  // Store search text
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div>
-      <h1>Personal Project Showcase App</h1>
+      {/* App heading */}
+      <Header />
+      {/* Form for adding projects */}
+      <ProjectForm
+      projects={projects}
+      setProjects={setProjects}
+      />
+      {/* Search input */}
+      <SearchBar
+      searchTerm={searchTerm}
+      setSearchTerm={setSearchTerm}
+      />
+      {/* Displays project cards */}
+      <ProjectList
+      projects={projects}
+      />
     </div>
   );
 }
